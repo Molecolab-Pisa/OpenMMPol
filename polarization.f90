f374e45f12744bc9f6ebf1545f4988d0750017ed
Vladislav Slama <slama@karlov.mff.cuni.cz>
2021-04-15 19:24:06 +0200

    Corrected calculation of the induced dipoles for the amoeba FF

--- polarization.f90
+++ polarization.f90
@@ -39,9 +39,11 @@ real(rp), dimension(3,pol_atoms,n_ipd), intent(out) :: ipds
 !
 ! internal variables:
 !
-real(rp),dimension(3*pol_atoms) :: e_vec, ipd0
+real(rp),dimension(3*pol_atoms) :: ep_vec, ed_vec, ipd0_p, ipd0_d
 integer(ip) :: I,n
 logical :: status
+!
+integer(ip), parameter :: diis_max = 20, norm_jacobi = 2
 
 !
 ! Dimension of the system
@@ -61,56 +63,45 @@ call create_TMat(TMat)
 !
 ! Reshape electric field matrix into a vector
 !
+ed_vec = RESHAPE(e(:,:,1), (/3*pol_atoms/))    ! the direct field for Amoeba FF or AMBER
 if (Amoeba) then
-    e_vec = RESHAPE(e(:,:,2), (/3*pol_atoms/))    ! the polarization field for Amoeba FF
-else
-    e_vec = RESHAPE(e(:,:,1), (/3*pol_atoms/))
+    ep_vec = RESHAPE(e(:,:,2), (/3*pol_atoms/))    ! the polarization field for Amoeba FF
 end if 
     
 !
-! Initial guess for iterative solvers 
+! Initial guess for iterative solvers (zero for matrix inversion)
 !
-ipd0 = Zero
+ipd0_p = Zero   ! Initial guess for amoeba p dipoles
+ipd0_d = Zero   ! Initial guess for amber or amoeba d dipoles
 if (iscr.le.2) then
-    call PolVec(3*pol_atoms,ipd0,ipd0)
-!    do I = 1, pol_atoms
-!        ipd0(3*I-2) = pol(I)*e(1,I,1)   ! ipd0x
-!        ipd0(3*I-1) = pol(I)*e(2,I,1)   ! ipd0y
-!        ipd0(3*I)   = pol(I)*e(3,I,1)   ! ipd0z
-!    enddo
+    call PolVec(3*pol_atoms,ed_vec,ipd0_d)
+    if (Amoeba) call PolVec(3*pol_atoms,ep_vec,ipd0_p)
 end if
-        
 !
 ! Decide which solver to use for computing induced point dipoles 
 !
 if  (iscr.eq.1) then            ! preconditioned conjugate gradient
-    call conjugate_gradient(n,verbose,convergence,e_vec,ipd0,nmax,status,TMatVec,PolVec)
+    call conjugate_gradient(n,verbose,convergence,ed_vec,ipd0_d,nmax,status,TMatVec,PolVec)
+    if (Amoeba) call conjugate_gradient(n,verbose,convergence,ep_vec,ipd0_p,nmax,status,TMatVec,PolVec)
 elseif (iscr.eq.2) then         ! jacobi iterations with DIIS extrapolation
-    call jacobi_diis(n,verbose,10,2,convergence,e_vec,ipd0,nmax,status,TMatVec_offdiag,PolVec)
+    call jacobi_diis(n,verbose,diis_max,norm_jacobi,convergence,ed_vec,ipd0_d,nmax,status,TMatVec_offdiag,PolVec)
+    if (Amoeba) call jacobi_diis(n,verbose,diis_max,norm_jacobi,convergence,ep_vec,ipd0_p,nmax,status,TMatVec_offdiag,PolVec)
 elseif (iscr.eq.3) then         !  matrix inversion
-    call induce_dipoles_MatInv(e_vec,ipd0)  !TODO: read polarization matrix TMat and not calculate it separately
+    call induce_dipoles_MatInv(ed_vec,ipd0_d)  !TODO: read polarization matrix TMat and not calculate it separately
+    if (Amoeba) call induce_dipoles_MatInv(ep_vec,ipd0_p)
 else
     write(*,*) "Unknown solver for calculation of the induced point dipoles in polarization"
 end if
-        
-!        
-! For Amoeba FF compute induced dipoles by the direct field
-!
-if (Amoeba) then
-    do I=1,pol_atoms
-        ipd( 1, I, 1) = pol(I)*e(1,I,1)
-        ipd( 2, I, 1) = pol(I)*e(2,I,1)
-        ipd( 3, I, 1) = pol(I)*e(3,I,1)
-    enddo
-end if
+
     
 !
 ! Reshape dipole vector into the matrix 
 !
 if (Amoeba) then
-    ipds( :, :, 2) = RESHAPE(ipd0, (/3,pol_atoms/)) 
+    ipds( :, :, 2) = RESHAPE(ipd0_p, (/3,pol_atoms/)) 
+    ipds( :, :, 1) = RESHAPE(ipd0_d, (/3,pol_atoms/)) 
 else
-    ipds( :, :, 1) = RESHAPE(ipd0, (/3,pol_atoms/)) 
+    ipds( :, :, 1) = RESHAPE(ipd0_d, (/3,pol_atoms/)) 
 end if
 
 !
