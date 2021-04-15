abeea0a10bc1a6814e81153d9910a1d06762c37c
Vladislav Slama <slama@karlov.mff.cuni.cz>
2021-04-15 11:57:17 +0200

    Added parameter f105

--- mmpol.f90
+++ mmpol.f90
@@ -226,7 +226,7 @@ module mmpol
 !
   real(rp),    allocatable :: v_qq(:,:), dv_qq(:,:)
 !
-!     field at the ipd and its derivatives
+!     field of the charges (multipoles) at the ipd and its derivatives
 !
   real(rp),    allocatable :: ef_qd(:,:,:), def_qd(:,:,:)
 !
@@ -249,7 +249,7 @@ module mmpol
   real(rp),    parameter   :: thres = 1.0e-8_rp
   real(rp),    parameter   :: zero = 0.0_rp, pt5 = 0.5_rp, one = 1.0_rp, two = 2.0_rp, three = 3.0_rp, &
                               four = 4.0_rp, five = 5.0_rp, six = 6.0_rp, seven = 7.0_rp, nine = 9.0_rp, &
-                              ten = 10.0_rp
+                              ten = 10.0_rp, f15 = 15.0_rp, f105 = 105.0_rp
 !
 ! -------------------------------------------------------------------------------------------------------
 !
