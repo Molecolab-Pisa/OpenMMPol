d87fc270d715ffc984c9dcf87598c69895350ca5
Mattia Bondanza <mattia.bondanza@unipi.it>
2022-08-29 12:32:09 +0200

    Python interface updating

--- python/pyopenmmpol/pyopenmmpol/__init__.py
+++ python/pyopenmmpol/pyopenmmpol/__init__.py
@@ -1,4 +1,4 @@
 from .pyopenmmpol import *
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 __author__ = 'Molecolab Group'
--- python/pyopenmmpol/pyopenmmpol/pyopenmmpol.py
+++ python/pyopenmmpol/pyopenmmpol/pyopenmmpol.py
@@ -13,60 +13,133 @@ if _libopenmmpol.__use_8bytes_int():
 else:
     INT_TYPE = ct.c_int32
 
-_libopenmmpol.w_mmpol_init.argtypes = [ct.c_char_p]
-_libopenmmpol.w_mmpol_init.restypes = []
-def w_mmpol_init(infile_mmp):
+_libopenmmpol.mmpol_init_mmp.argtypes = [ct.c_char_p]
+_libopenmmpol.mmpol_init_mmp.restypes = []
+def ommp_init_mmp(infile_mmp):
     buf = ct.create_string_buffer(infile_mmp.encode())
-    _libopenmmpol.w_mmpol_init(buf)
+    _libopenmmpol.mmpol_init_mmp(buf)
 
-_libopenmmpol.do_mm.argtypes = []
-_libopenmmpol.do_mm.restypes = []
-def do_mm():
-    _libopenmmpol.do_mm()
+_libopenmmpol.mmpol_init_xyz.argtypes = [ct.c_char_p]
+_libopenmmpol.mmpol_init_xyz.restypes = []
+def ommp_init_xyz(infile_prm, infile_xyz):
+    bufprm = ct.create_string_buffer(infile_prm.encode())
+    bufxyz = ct.create_string_buffer(infile_xyz.encode())
+    _libopenmmpol.mmpol_init_xyz(bufxyz, bufprm)
 
-_libopenmmpol.do_qmmm.restypes = []
-def do_qmmm(vqm, eqm):
+_libopenmmpol.ommp_terminate.argtypes = []
+_libopenmmpol.ommp_terminate.restypes = []
+def ommp_terminate():
+    _libopenmmpol.ommp_terminate()
+
+# ------
+
+def set_external_field(eqm, solver):
     _eqm = np.ascontiguousarray(eqm)
-    _vqm = np.ascontiguousarray(vqm)
-    vqm_type = npct.ndpointer(dtype=ct.c_double,
-                              ndim=2,
-                              shape=(get_mm_atoms(),
-                                     get_ld_cart()),
-                                     flags='C_CONTIGUOUS')
     eqm_type = npct.ndpointer(dtype=ct.c_double,
-                              ndim=3,
-                              shape=(get_n_ipd(),
-                                     get_pol_atoms(),
+                              ndim=2,
+                              shape=(get_pol_atoms(),
                                      3),
-                                     flags='C_CONTIGUOUS')
-    _libopenmmpol.do_qmmm.argtypes = [vqm_type, 
-                                      eqm_type,
-                                      INT_TYPE,
-                                      INT_TYPE,
-                                      INT_TYPE,
-                                      INT_TYPE]
-                                                  
-    _libopenmmpol.do_qmmm(_vqm, 
-                          _eqm,
-                          get_ld_cart(),
-                          get_mm_atoms(),
-                          get_pol_atoms(),
-                          get_n_ipd())
+                              flags='C_CONTIGUOUS')
 
-_libopenmmpol.restart.argtypes = []
-_libopenmmpol.restart.restypes = []
-def restart():
-    _libopenmmpol.restart()
+    _libopenmmpol.set_external_field.restypes = []
+    _libopenmmpol.set_external_field.argtypes = [eqm_type, INT_TYPE]
+    _libopenmmpol.set_external_field(_eqm, ???)
 
-_libopenmmpol.get_energy.argtypes = [ct.POINTER(ct.c_double), 
-                                     ct.POINTER(ct.c_double)]
-_libopenmmpol.get_energy.restypes = []
-def get_energy():
-    EMM = ct.c_double(0.0)
+
+_libopenmmpol.get_polelec_energy.argtypes = [ct.POINTER(ct.c_double)]
+_libopenmmpol.get_polelec_energy.restypes = []
+def get_polelec_energy():
     EPol = ct.c_double(0.0)
-    _libopenmmpol.get_energy(ct.byref(EMM), 
-                             ct.byref(EPol))
-    return EMM.value, EPol.value
+    _libopenmmpol.get_polelec_energy(ct.byref(EPol))
+    return EPol.value
+
+_libopenmmpol.get_fixedelec_energy.argtypes = [ct.POINTER(ct.c_double)]
+_libopenmmpol.get_fixedelec_energy.restypes = []
+def get_fixedelec_energy():
+    EMM = ct.c_double(0.0)
+    _libopenmmpol.get_fixedelec_energy(ct.byref(EMM))
+    return EMM.value
+
+# -------
+
+_libopenmmpol.get_vdw_energy.argtypes = [ct.POINTER(ct.c_double)]
+_libopenmmpol.get_vdw_energy.restype = []
+def get_vdw_energy():
+    ev = ct.c_double(0.0)
+    _libopenmmpol.get_vdw_energy(ct.byref(ev))
+    return ev
+
+_libopenmmpol.get_bond_energy.argtypes = [ct.POINTER(ct.c_double)]
+_libopenmmpol.get_bond_energy.restype = []
+def get_bond_energy():
+    eb = ct.c_double(0.0)
+    _libopenmmpol.get_bond_energy(ct.byref(eb))
+    return eb
+
+_libopenmmpol.get_angle_energy.argtypes = [ct.POINTER(ct.c_double)]
+_libopenmmpol.get_angle_energy.restype = []
+def get_angle_energy():
+    ea = ct.c_double(0.0)
+    _libopenmmpol.get_angle_energy(ct.byref(ea))
+    return ea
+
+_libopenmmpol.get_angtor_energy.argtypes = [ct.POINTER(ct.c_double)]
+_libopenmmpol.get_angtor_energy.restype = []
+def get_angtor_energy():
+    eat = ct.c_double(0.0)
+    _libopenmmpol.get_angtor_energy(ct.byref(eat))
+    return eat
+
+_libopenmmpol.get_strtor_energy.argtypes = [ct.POINTER(ct.c_double)]
+_libopenmmpol.get_strtor_energy.restype = []
+def get_strtor_energy():
+    ebt = ct.c_double(0.0)
+    _libopenmmpol.get_strtor_energy(ct.byref(ebt))
+    return ebt
+
+_libopenmmpol.get_strbnd_energy.argtypes = [ct.POINTER(ct.c_double)]
+_libopenmmpol.get_strbnd_energy.restype = []
+def get_strbnd_energy():
+    eba = ct.c_double(0.0)
+    _libopenmmpol.get_strbnd_energy(ct.byref(eba))
+    return eba
+
+_libopenmmpol.get_opb_energy.argtypes = [ct.POINTER(ct.c_double)]
+_libopenmmpol.get_opb_energy.restype = []
+def get_opb_energy():
+    eopb = ct.c_double(0.0)
+    _libopenmmpol.get_opb_energy(ct.byref(eopb))
+    return eopb
+
+_libopenmmpol.get_pitors_energy.argtypes = [ct.POINTER(ct.c_double)]
+_libopenmmpol.get_pitors_energy.restype = []
+def get_pitors_energy():
+    ept = ct.c_double(0.0)
+    _libopenmmpol.get_pitors_energy(ct.byref(ept))
+    return ept
+
+_libopenmmpol.get_torsion_energy.argtypes = [ct.POINTER(ct.c_double)]
+_libopenmmpol.get_torsion_energy.restype = []
+def get_torsion_energy():
+    et = ct.c_double(0.0)
+    _libopenmmpol.get_torsion_energy(ct.byref(et))
+    return et
+
+_libopenmmpol.get_tortor_energy.argtypes = [ct.POINTER(ct.c_double)]
+_libopenmmpol.get_tortor_energy.restype = []
+def get_tortor_energy():
+    ett = ct.c_double(0.0)
+    _libopenmmpol.get_tortor_energy(ct.byref(et))
+    return ett
+
+_libopenmmpol.get_urey_energy.argtypes = [ct.POINTER(ct.c_double)]
+_libopenmmpol.get_urey_energy.restype = []
+def get_urey_energy():
+    eub = ct.c_double(0.0)
+    _libopenmmpol.get_urey_energy(ct.byref(eub))
+    return eub
+
+# -------
 
 _libopenmmpol.get_n_ipd.argtypes = []
 _libopenmmpol.get_n_ipd.restype = INT_TYPE
@@ -88,18 +161,13 @@ _libopenmmpol.get_pol_atoms.restype = INT_TYPE
 def get_pol_atoms():
     return int(_libopenmmpol.get_pol_atoms())
 
-_libopenmmpol.is_amoeba.argtypes = []
-_libopenmmpol.is_amoeba.restype = ct.c_bool
-def is_amoeba():
-     return bool(_libopenmmpol.is_amoeba())
-
 _libopenmmpol.get_cmm.argtypes = []
 def get_cmm():
     rtype = npct.ndpointer(dtype=ct.c_double,
                            ndim=2,
                            shape=(get_mm_atoms(),3),
                            flags=('F_CONTIGUOUS'))
-    _libopenmmpol.get_cmm.restype = rtype 
+    _libopenmmpol.get_cmm.restype = rtype
     cmm = _libopenmmpol.get_cmm()
     return cmm
 
@@ -109,7 +177,7 @@ def get_cpol():
                            ndim=2,
                            shape=(get_pol_atoms(),3),
                            flags=('F_CONTIGUOUS'))
-    _libopenmmpol.get_cpol.restype = rtype 
+    _libopenmmpol.get_cpol.restype = rtype
     cpol = _libopenmmpol.get_cpol()
     return cpol
 
@@ -119,7 +187,7 @@ def get_q():
                            ndim=2,
                            shape=(get_mm_atoms(), get_ld_cart()),
                            flags=('F_CONTIGUOUS'))
-    _libopenmmpol.get_q.restype = rtype 
+    _libopenmmpol.get_q.restype = rtype
     q = _libopenmmpol.get_q()
     return q
 
@@ -129,146 +197,14 @@ def get_ipd():
                            ndim=3,
                            shape=(get_n_ipd(), get_pol_atoms(), 3),
                            flags=('F_CONTIGUOUS'))
-    _libopenmmpol.get_ipd.restype = rtype 
+    _libopenmmpol.get_ipd.restype = rtype
     ipd = _libopenmmpol.get_ipd()
     return ipd
 
-class MMPol(object):
-  def __init__(self, mmpol_file):
-   
-    if (get_mm_atoms() > 0):
-      raise( RuntimeError('\n MMPol Module was already initialized! '))
+# get_polar_mm
 
-    w_mmpol_init(mmpol_file)
-
-    pass
-
-  @property
-  def is_amoeba(self):
-    return is_amoeba()
-
-  @property
-  def n_ipd(self):
-    return get_n_ipd()
-
-  @property
-  def ld_cart(self):
-    return get_ld_cart()
-
-  @property
-  def mm_atoms(self):
-    return get_mm_atoms()
-
-  @property
-  def pol_atoms(self):
-    return get_pol_atoms()
-
-  @property
-  def cmm(self):
-    return get_cmm()
-
-  @property
-  def cpol(self):
-    return get_cpol()
-
-  @property
-  def q(self):
-    return get_q()
-
-  @property
-  def ipd(self):
-    return get_ipd()
-
-
-  def do_mm(self):
-    do_mm()
-
-    
-  def restart(self):
-    restart()
-    
-
-  def do_qmmm(self,VQM,EQM):
-
-    if VQM.ndim == 1:
-      VQM = VQM[:,None] # ld_cart = 1
-    if EQM.ndim == 2:
-      EQM = EQM[None,:] # n_ipd = 1
-    
-    # Check shapes
-    assert np.shape(VQM) == (self.mm_atoms,self.ld_cart)
-    assert np.shape(EQM) == (self.n_ipd,self.pol_atoms,3)
-
-    do_qmmm(VQM,EQM)
-
-  def get_MM_energy(self):
-
-    self.EMM, self.EMMPol = get_energy()
-
-    print(" ---------------------------------------------")
-    print("   MM     -- MM (q)    {:14.4f} kcal/mol".format(self.EMM*627.5094740630558))
-    print("   MM     -- MM (dip)  {:14.4f} kcal/mol".format(self.EMMPol*627.5094740630558))
-    print(" Total MM Energy       {:14.4f} kcal/mol".format((self.EMM+self.EMMPol)*627.5094740630558))
-
-    return self.EMM,self.EMMPol
-
-  def get_QMMM_energy(self,VQM,EQM):
-
-    if VQM.ndim == 1:
-      VQM = VQM[:,None] # ld_cart = 1
-    if EQM.ndim == 2:
-      EQM = EQM[None,:] # n_ipd = 1
-    
-    # Check shapes
-    assert np.shape(VQM) == (self.mm_atoms,self.ld_cart)
-    assert np.shape(EQM) == (self.n_ipd,self.pol_atoms,3)
-
-    # Potential * multipoles
-    self.e_static = np.sum(VQM*self.q)
-
-    # Field * induced dipoles 
-    self.e_polar  = -0.5 * np.sum(EQM*self.ipd)
-
-    e = self.e_static + self.e_polar
-    print(" ---------------------------------------------")
-    print("   QM     -- MM (q)    {:14.4f} kcal/mol".format(self.e_static*627.5094740630558))
-    print("   QM     -- MM (dip)  {:14.4f} kcal/mol".format(self.e_polar*627.5094740630558))
-    print(" Total QM/MM Energy    {:14.4f} kcal/mol".format(e*627.5094740630558))
-
-
-    return self.e_static,self.e_polar
-
-
-#### For testing
-# if __name__ == '__main__':
-# 
-#   # Initialize mmpol
-#   mmp = MMPol('test01.mmp') 
-# 
-# 
-#   # Get coordinates of MM part
-#   cmm  = mmp.cmm
-#   cpol = mmp.cpol
-# 
-#   # Do MM part
-#   mmp.do_mm()
-# 
-#   # Fake a potential and a field
-#   # (they should be calculated from QM density ...)
-#   V = -cmm[:,0]*10
-#   E = np.zeros(cpol.shape)
-#   E[:,0] = 1
-# 
-# #  EQMMM = np.dot(V,mmp.q[:,0])
-# #  print( EQMMM)
-# 
-#   print (V[:,None].T.shape)
-# 
-#   mmp.do_qmmm(V[:,None],E)
-# 
-#   print( mmpol.ipd.T )
-# 
-#   mmp.get_MM_energy()
-#   mmp.get_QMMM_energy(V[:,None],E)
-#   quit()
+_libopenmmpol.is_amoeba.argtypes = []
+_libopenmmpol.is_amoeba.restype = ct.c_bool
+def is_amoeba():
+     return bool(_libopenmmpol.is_amoeba())
 
