80111f1224395ce70c509fc32565bf7201e545c6
Mattia Bondanza <mattia.bondanza@unipi.it>
2022-08-31 11:41:39 +0200

    Added some test script in python

---
+++ src/test_programs/test_energy.py
@@ -0,0 +1,57 @@
+import numpy as np
+import pyopenmmpol as ommp
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--verbose", "-v",
+                    help="Increase verbosity level to debug",
+                    action="store_true")
+parser.add_argument("--mmpol", "-i",
+                    help=".mmpol file to be used as input",
+                    required=True)
+parser.add_argument("--electric-field", "-f",
+                    help="""File containing a formatted matrix (3xN)
+                         with N number of induced dipoles,
+                         containing the electric field at
+                         polarizable sites""")
+parser.add_argument("--out-file", "-o",
+                    help="""Formatted file where to print the
+                            results""", default=None)
+parser.add_argument("--solver", "-s",
+                    help="""Solver method for the linear
+                            system""",
+                    default='default',
+                    choices=ommp.available_solvers.keys())
+
+args = parser.parse_args()
+
+ext_field_file = args.electric_field
+infile = args.mmpol
+outfile = args.out_file
+if(args.verbose):
+    ommp.set_verbose(3)
+else:
+    ommp.set_verbose(1)
+
+ommp.init_mmp(infile)
+
+if ext_field_file is not None:
+    ef = np.loadtxt(ext_field_file)
+else:
+    ef = np.zeros((ommp.get_pol_atoms(), 3))
+
+ommp.set_external_field(ef, 'cg')
+em = ommp.get_fixedelec_energy()
+ep = ommp.get_polelec_energy()
+
+if outfile is None:
+    print('{:20.12f}'.format(em))
+    print('{:20.12f}'.format(ep))
+else:
+    with open(outfile, 'w') as f:
+        print('{:20.12f}'.format(em), file=f)
+        print('{:20.12f}'.format(ep), file=f)
+
+
+
---
+++ src/test_programs/test_ipd.py
@@ -0,0 +1,62 @@
+import numpy as np
+import pyopenmmpol as ommp
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--verbose", "-v",
+                    help="Increase verbosity level to debug",
+                    action="store_true")
+parser.add_argument("--mmpol", "-i",
+                    help=".mmpol file to be used as input",
+                    required=True)
+parser.add_argument("--electric-field", "-f",
+                    help="""File containing a formatted matrix
+                         (3xN) with N number of induced dipoles,
+                         containing the electric field at
+                         polarizable sites""")
+parser.add_argument("--out-file", "-o",
+                    help="""Formatted file where to print the
+                            results""", default=None)
+parser.add_argument("--solver", "-s",
+                    help="""Solver method for the linear
+                            system""",
+                    default='default',
+                    choices=ommp.available_solvers.keys())
+
+args = parser.parse_args()
+
+ext_field_file = args.electric_field
+infile = args.mmpol
+outfile = args.out_file
+solver = args.solver
+if(args.verbose):
+    ommp.set_verbose(3)
+else:
+    ommp.set_verbose(1)
+
+ommp.init_mmp(infile)
+
+if ext_field_file is not None:
+    ef = np.loadtxt(ext_field_file)
+else:
+    ef = np.zeros((ommp.get_pol_atoms(), 3))
+
+ommp.set_external_field(ef, solver)
+ipd = ommp.get_ipd()
+
+if outfile is None:
+    for k in range(ipd.shape[0]):
+        for i in range(ipd.shape[1]):
+            print('{:20.12f}{:20.12f}{:20.12f}'.format(ipd[k,i,0],
+                                                       ipd[k,i,1],
+                                                       ipd[k,i,2]))
+else:
+    with open(outfile, 'w') as f:
+        for k in range(ipd.shape[0]):
+            for i in range(ipd.shape[1]):
+                print('{:20.12f}{:20.12f}{:20.12f}'.format(ipd[k,i,0],
+                                                           ipd[k,i,1],
+                                                           ipd[k,i,2]),
+                      file=f)
+
