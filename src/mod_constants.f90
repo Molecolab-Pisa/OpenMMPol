83f7c4bebc9fcbd2d44276a55dc560254f5afc13
Mattia Bondanza <mattia.bondanza@unipi.it>
2022-09-14 12:20:00 +0200

    Documentation of mod_constants improved

--- src/mod_constants.f90
+++ src/mod_constants.f90
@@ -1,14 +1,25 @@
 module mod_constants
+    !! Contains several constants that are usefoul around the code. There are 
+    !! physical constants (there should be no duplicate around!), default 
+    !! force-field parameters that are used when no other input is specified, 
+    !! and internal constants that are used as easy-to-remember names of code
+    !! constants.
     use mod_memory, only: ip, rp
     implicit none
   
     ! Physical constants
-    real(rp), parameter :: angstrom2au = 1.8897261245650_rp, &
-                           kcalmol2au = 1.59360109742136e-3_rp, &
-                           au2kcalmol = 627.5096080306_rp, &
-                           pi = 4.0 * atan(1.0), &
-                           deg2rad = pi / 180.0, &
-                           rad2deg = 180.0 / pi
+    real(rp), parameter :: angstrom2au = 1.8897261245650_rp
+    !! Conversion factor from \(\AA\) to A.U.
+    real(rp), parameter :: kcalmol2au = 1.59360109742136e-3_rp
+    !! Conversion factor from kcal mol\(^{-1}\) to A.U.
+    real(rp), parameter :: au2kcalmol = 627.5096080306_rp
+    !! Conversion factor from A.U. to kcal mol\(^{-1}\)
+    real(rp), parameter :: pi = 4.0 * atan(1.0)
+    !! Value of \(\pi\)
+    real(rp), parameter :: deg2rad = pi / 180.0
+    !! Conversion from arc degree to radians
+    real(rp), parameter :: rad2deg = 180.0 / pi
+    !! Conversion from radians to arc degree
 
     ! Force Field parameters
     real(rp), parameter :: mscale_wang_al(4) = [0.0_rp, 0.0_rp, 1.0_rp, 1.0_rp]
@@ -29,8 +40,10 @@ module mod_constants
                                               [0.0_rp, 0.0_rp, 0.5_rp, 1.0_rp]
 
             
-    real(rp), parameter :: thole_scale_wang_al = 2.5874_rp, &
-                           thole_scale_wang_dl = 2.0580_rp
+    real(rp), parameter :: thole_scale_wang_al = 2.5874_rp
+    !! Default Thole scaling factor used in Wang-AL force field
+    real(rp), parameter :: thole_scale_wang_dl = 2.0580_rp
+    !! Default Thole scaling factor used in Wang-DL force field
     
     ! Code constants
     real(rp), parameter :: eps_rp = epsilon(0.0_rp) * 100
@@ -69,9 +82,13 @@ module mod_constants
     !! Index of polarization (P) field and dipoles in AMOEBA FF
 
     integer(ip), parameter :: OMMP_VERBOSE_DEBUG = 3_ip
+    !! Maximum verbosity level allowed
     integer(ip), parameter :: OMMP_VERBOSE_HIGH = 2_ip
+    !! High level of verbosity
     integer(ip), parameter :: OMMP_VERBOSE_LOW = 1_ip
+    !! Normal verbosity level
     integer(ip), parameter :: OMMP_VERBOSE_NONE = 0_ip
+    !! All output should be suppressed at this level
 
     integer(ip), parameter :: AMOEBA_ROT_NONE = 0_ip 
     integer(ip), parameter :: AMOEBA_ROT_Z_THEN_X = 1_ip 
