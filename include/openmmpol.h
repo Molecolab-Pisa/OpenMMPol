66968827dde47f0233106b06e37007f81f1dd6e1
Mattia Bondanza <mattia.bondanza@unipi.it>
2023-01-10 12:08:17 +0100

    Added test geomgrad in C

--- include/openmmpol.h
+++ include/openmmpol.h
@@ -78,10 +78,8 @@ extern int32_t *ommp_get_polar_mm(OMMP_SYSTEM_PRT);
 
 extern void ommp_update_coordinates(OMMP_SYSTEM_PRT, const double *);
 
-extern void ommp_fixedelec_numgeomgrad(OMMP_SYSTEM_PRT, double *);
-extern void ommp_fixedelec_anageomgrad(OMMP_SYSTEM_PRT, double *);
-extern void ommp_polelec_numgeomgrad(OMMP_SYSTEM_PRT, double *);
-extern void ommp_polelec_anageomgrad(OMMP_SYSTEM_PRT, double *);
+extern void ommp_fixedelec_geomgrad(OMMP_SYSTEM_PRT, double *);
+extern void ommp_polelec_geomgrad(OMMP_SYSTEM_PRT, double *);
 
 #ifdef __cplusplus
 }
