603be481eee35159186af83e29e84bdf588aae38
Mattia Bondanza <mattia.bondanza@inventati.org>
2023-02-14 16:20:01 +0100

    Added support for null/free torsion

--- src/mod_prm.f90
+++ src/mod_prm.f90
@@ -1212,7 +1212,7 @@ module mod_prm
         integer(ip), allocatable :: classa(:), classb(:), classc(:), classd(:), &
                                     t_n(:,:), tmpat(:,:), tmpprm(:)
         real(rp), allocatable :: t_amp(:,:), t_pha(:,:)
-        real(rp) :: amp, phase, torsion_unit
+        real(rp) :: amp, phase, torsion_unit = 1.0
         type(ommp_topology_type), pointer :: top
 
         top => bds%top
@@ -1253,7 +1253,9 @@ module mod_prm
         call mallocate('assign_torsion [t_n]', 6, nt, t_n)
         call mallocate('assign_torsion [tmpat]', 4, maxt, tmpat)
         call mallocate('assign_torsion [tmpprm]', maxt, tmpprm)
-
+        t_amp = 0.0
+        t_pha = 0.0
+        t_n = 1
         ! Restart the reading from the beginning to actually save the parameters
         rewind(iof_prminp)
         ist = 0
@@ -1317,7 +1319,6 @@ module mod_prm
                         call fatal_error(errstring)
                     end if
                     read(line(tokb:toke), *) amp
-                    
                     tokb = toke + 1
                     toke = tokenize(line, tokb)
                     if(.not. isreal(line(tokb:toke))) then
@@ -1430,7 +1431,7 @@ module mod_prm
         integer(ip), allocatable :: classa(:), classb(:), classc(:), classd(:), &
                                     tmpat(:,:), tmpprm(:)
         real(rp), allocatable :: kat(:,:)
-        real(rp) :: phase, torsion_unit
+        real(rp) :: phase, torsion_unit = 1.0
         logical :: tor_done, bnd1_done, bnd2_done, bnd3_done
         type(ommp_topology_type), pointer :: top
 
