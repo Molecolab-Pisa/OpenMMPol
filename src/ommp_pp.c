86982a4d782b9b727630c27653f88414109c32da
Mattia Bondanza <mattia.bondanza@unipi.it>
2023-10-11 10:40:55 +0200

    ommp_pp now works with smartinput

--- src/ommp_pp.c
+++ src/ommp_pp.c
@@ -5,18 +5,23 @@
 #include "openmmpol.h"
 
 int main(int argc, char **argv){
-    if(argc != 4){
+    if(argc != 3){
         printf("Syntax expected\n");
-        printf("    $ test_init_xyz.exe <XYZ FILE> <PRM FILE> <OMMP HDF5 INPUT FILE>\n");
+        printf("    $ ommp_pp <JSON SI FILE> <OMMP HDF5 INPUT FILE>\n");
         return 0;
     }
     
     ommp_set_verbose(OMMP_VERBOSE_LOW);
-    OMMP_SYSTEM_PRT my_system = ommp_init_xyz(argv[1], argv[2]);
+    OMMP_SYSTEM_PRT my_system;
+    OMMP_QM_HELPER_PRT my_qmh;
+    ommp_smartinput(argv[1], &my_system, &my_qmh);
+    if(my_qmh != NULL){
+        printf("Only MM part of the system could be saved in HDF5 format, please remove all the section referring to QM.\n");
+        return 1;
+    }
 
-    ommp_save_as_hdf5(my_system, argv[3], "system");
-    
-    ommp_terminate(my_system);
+    ommp_save_as_hdf5(my_system, argv[2], "system");
+    if(my_system != NULL) ommp_terminate(my_system);
     
     return 0;
 }
