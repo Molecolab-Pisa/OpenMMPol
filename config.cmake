a95d2e8c2a038bba0fa1d5d4b7096270d1a26b42
Mattia Bondanza <mattia.bondanza@unipi.it>
2024-06-28 10:04:51 +0200

    Build target for test programs, tests enabled by default

--- config.cmake
+++ config.cmake
@@ -5,4 +5,4 @@ option(WITH_HDF5 "Whether to use HDF5" TRUE)
 option(WITH_PYTHON "Whether Python interface should be built and installed" FALSE)
 
 # Tests
-option(WITH_TESTS "Whether tests should be compiled" FALSE)
+option(WITH_TESTS "Whether tests should be compiled" TRUE)
