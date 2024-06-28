ef116d56dacbcac634c61b75315fe37adbc95341
Mattia Bondanza <mattia.bondanza@unipi.it>
2024-06-28 08:56:58 +0200

    Changed  std=2018 with std=2008ts which is supported by older GNU compilers and acts as an alias on newer ones.

--- toolchain/gnu.cmake
+++ toolchain/gnu.cmake
@@ -10,7 +10,7 @@ set(Fortran_FLAGS_RELEASE "-O3 -funroll-all-loops"
 set(Fortran_FLAGS_RELWITHDEBINFO "-g ${Fortran_FLAGS_RELEASE}"
   CACHE STRING "Fortran compiler flags for Release build")
 
-set(Fortran_FLAGS_DEBUG "-g -Wall -std=f2018 -fbounds-check "
+set(Fortran_FLAGS_DEBUG "-g -Wall -std=f2008ts -fbounds-check "
   CACHE STRING "Fortran compiler flags for Debug build")
 
 set(Fortran_FLAGS_COVERAGE "-O0 -g --coverage")
